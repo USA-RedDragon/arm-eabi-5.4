ec09333c4ae21657cfb42b54e7b390812ade9973
Jacob McSwain <jacob.a.mcswain@gmail.com>
2016-06-10 15:19:24 -0500

    GCC 5.4.1 built on 10 June 2016

---
+++ lib/gcc/arm-eabi/5.4.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-upstream/configure --prefix=/root/gcc5.4 --target=arm-eabi --enable-languages=c,c++ --disable-multilib --with-gnu-as --with-newlib";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
